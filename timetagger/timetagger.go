109efbaabbf798c2930e881590191b11f7d7f248
Hunmin Hwang <hunmin@implit.co.kr>
2019-07-22 18:50:04 +0900

    * ANSI 컬러 처리 * 라인수 표시

--- timetagger/timetagger.go
+++ timetagger/timetagger.go
@@ -3,18 +3,25 @@ package timetagger
 import (
 	"fmt"
 	"time"
+
+	"github.com/fatih/color"
 )
 
-type TimeTagger struct {
-	start  time.Time
-	preTag string
+var (
+	colorGreenBold = color.New(color.FgGreen, color.Bold)
+)
+
+type timeTagger struct {
+	start    time.Time
+	preTag   string
+	sameLine int
 }
 
-func NewTimeTagger() *TimeTagger {
-	return &TimeTagger{start: time.Now()}
+func NewTimeTagger() *timeTagger {
+	return &timeTagger{start: time.Now()}
 }
 
-func (tt *TimeTagger) Tag() string {
+func (tt *timeTagger) Tag() string {
 	diff := time.Now().Sub(tt.start).Round(time.Second)
 	hour := diff / time.Hour
 
@@ -24,12 +31,14 @@ func (tt *TimeTagger) Tag() string {
 	diff -= (min * time.Minute)
 	sec := diff / time.Second
 
-	tag := fmt.Sprintf("%02d:%02d:%02d", hour, min, sec)
+	tag := colorGreenBold.Sprintf(fmt.Sprintf("%02d:%02d:%02d", hour, min, sec))
 
 	if tag != tt.preTag {
 		tt.preTag = tag
+		tt.sameLine = 1
 	} else {
-		tag = ""
+		tt.sameLine++
+		tag = color.GreenString(fmt.Sprintf("%+8d", tt.sameLine))
 	}
 
 	return tag
